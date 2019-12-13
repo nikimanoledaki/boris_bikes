f2dc7d99f17fcb28d079569def07435b568509e8
niki-01 <niki.manoledaki@gmail.com>
2019-12-13 16:46:50 +0000

    Initializes with DEFAULT_CAPACITY

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,21 +2,12 @@ require_relative 'bike'
 
 class DockingStation
   DEFAULT_CAPACITY = 20
-<<<<<<< HEAD
   
   attr_reader :capacity
-=======
-  attr_reader :capacity, :bikes
-
->>>>>>> 60481ac99945399bf0c272e9e1a8278b8b4af9c7
 
   def initialize(capacity=DEFAULT_CAPACITY)
     @bikes = []
     @capacity = capacity
-<<<<<<< HEAD
-=======
-    @capacity = DEFAULT_CAPACITY
->>>>>>> 60481ac99945399bf0c272e9e1a8278b8b4af9c7
   end
 
   def release_bike
@@ -28,17 +19,11 @@ class DockingStation
     fail "Dock is full" if full?
       bikes << bike
   end
-<<<<<<< HEAD
   
   private
 
   attr_reader :bikes
  
-=======
-
-  private
-
->>>>>>> 60481ac99945399bf0c272e9e1a8278b8b4af9c7
   def full?
     bikes.count >= capacity
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,34 +1,57 @@
-require_relative 'bike'
+require 'docking_station'
 
-class DockingStation
-  DEFAULT_CAPACITY = 20
-  attr_reader :capacity, :bikes
+describe DockingStation do 
+  
+  subject {DockingStation.new}
+  let(:bike) {Bike.new}
 
+  describe "Initializtion" do    
+    it 'default capacity' do
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(bike)}
+      expect{subject.dock(bike)}.to raise_error "No space available"
+    end
+  end 
 
-  def initialize(capacity=DEFAULT_CAPACITY)
-    @bikes = []
-    @capacity = capacity
-    @capacity = DEFAULT_CAPACITY
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end 
+
+  it { is_expected.to respond_to :dock }
+  it { is_expected.to respond_to(:dock).with(1).argument } 
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq [bike]
+  end 
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.dock(@bikes)).not_to be_empty
+  end 
+
+  describe 'release_bike' do
+    it 'release a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raised an error when there are no bikes available' do
+      expect{ subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
-  def release_bike
-    fail "No bikes available" if empty?
-      bikes.pop
+  describe 'dock' do
+    it "raised an error when full" do
+      subject.capacity.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error "No space available"
+    end
   end
 
-  def dock(bike)
-    fail "Dock is full" if full?
-      bikes << bike
-  end
-
-  private
-
-  def full?
-    bikes.count >= capacity
-  end
-
-  def empty?
-    bikes.empty?
-  end
-
-end
+end 
