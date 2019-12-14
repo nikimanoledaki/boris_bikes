dffe1f495e0e4710ac5bb39c4e7fb67a3558877a
niki-01 <niki.manoledaki@gmail.com>
2019-12-14 13:20:53 +0000

    Refactoring repetitive bike = Bike.new

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,6 @@ describe DockingStation do
 
   describe "Initializtion" do    
     it 'default capacity' do
-      bike = Bike.new
       DockingStation::DEFAULT_CAPACITY.times {subject.dock(bike)}
       expect{subject.dock(bike)}.to raise_error "No space available"
     end
@@ -16,7 +15,6 @@ describe DockingStation do
   describe 'release_bike' do
 
     it 'releases working bikes' do
-      bike = Bike.new
       subject.dock(bike)
       bike = subject.release_bike
       expect(bike).to be_working
@@ -25,7 +23,6 @@ describe DockingStation do
     it { is_expected.to respond_to :release_bike }
     
     it 'release a bike' do
-      bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -40,18 +37,15 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument } 
   
     it 'docks something' do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq [bike]
     end 
   
     it 'returns docked bikes' do
-      bike = Bike.new
       subject.dock(bike)
       expect(subject.dock(@bikes)).not_to be_empty
     end 
 
     it "raised an error when full" do
-      bike = Bike.new
       subject.capacity.times { subject.dock(bike) }
       expect { subject.dock(bike) }.to raise_error "No space available"
     end
